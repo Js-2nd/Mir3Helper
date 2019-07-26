f63086c820a172c03085d02cf317c464392f40b6
Js <JimmyLoveSiren@users.noreply.github.com>
2019-07-27 00:52:06 +0900

    PostToMainThread

---
+++ Mir3Helper/CustomWindowMessage.cs
@@ -0,0 +1,9 @@
+namespace Mir3Helper
+{
+	using PInvoke;
+
+	static class CustomWindowMessage
+	{
+		public const User32.WindowMessage PostAction = User32.WindowMessage.WM_USER;
+	}
+}
--- Mir3Helper/Program.cs
+++ Mir3Helper/Program.cs
@@ -2,30 +2,34 @@
 {
 	using PInvoke;
 	using System;
+	using System.Runtime.InteropServices;
 
 	static class Program
 	{
-		static int MainThreadId;
+		public static int MainThreadId { get; private set; }
+		public static InputSystem Input { get; private set; }
 
 		static void Main(string[] args)
 		{
 			MainThreadId = Kernel32.GetCurrentThreadId();
+			Input = new InputSystem();
 			MainAsync(args);
 			MessageLoop();
+//			Input.Dispose();
+//			Input.EventLoopTask.Wait();
 		}
 
 		static async void MainAsync(string[] args)
 		{
-			var input = new InputSystem();
 			while (true)
 			{
-				var key = await input.WaitKeyDown();
+				var key = await Input.WaitKeyDown();
 				if (key == User32.VirtualKey.VK_ESCAPE) break;
-				Console.WriteLine(key);
+				var window = User32.GetForegroundWindow();
+				string title = User32.GetWindowText(window);
+				Console.WriteLine(title);
 			}
 
-			input.Dispose();
-			await input.EventLoopTask;
 			User32.PostThreadMessage(MainThreadId, User32.WindowMessage.WM_QUIT, IntPtr.Zero, IntPtr.Zero);
 		}
 
@@ -34,10 +38,23 @@
 			User32.MSG msg;
 			while (User32.GetMessage(&msg, IntPtr.Zero, 0, 0) > 0)
 			{
-				Console.WriteLine($"[Message] {msg.message} {msg.wParam} {msg.lParam}");
-				User32.TranslateMessage(&msg);
-				User32.DispatchMessage(&msg);
+				switch (msg.message)
+				{
+					case CustomWindowMessage.PostAction:
+						Marshal.GetDelegateForFunctionPointer<Action>(msg.wParam).Invoke();
+						break;
+					default:
+						User32.TranslateMessage(&msg);
+						User32.DispatchMessage(&msg);
+						break;
+				}
 			}
+		}
+
+		public static void PostToMainThread(Action action)
+		{
+			User32.PostThreadMessage(MainThreadId, CustomWindowMessage.PostAction,
+				Marshal.GetFunctionPointerForDelegate(action), IntPtr.Zero);
 		}
 	}
 }
