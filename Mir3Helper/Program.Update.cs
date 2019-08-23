7db78c9749a4ec9cf6bebfb975afbc29c5419f19
Js <JimmyLoveSiren@users.noreply.github.com>
2019-08-23 19:27:21 +0900

    Program

--- Mir3Helper/Program.Update.cs
+++ Mir3Helper/Program.Update.cs
@@ -42,41 +42,23 @@
 
 			if (m_HasUser && dist <= 7 && m_User.Hp > 0)
 			{
-				if (TryCastBuff(m_Assist, Skill.强震魔法, m_User) ||
-				    TryCastBuff(m_Assist, Skill.神圣战甲术, m_User) ||
-				    TryCastBuff(m_Assist, Skill.幽灵盾, m_User) ||
-				    m_User.Class != PlayerClass.Mage && TryCastBuff(m_Assist, Skill.猛虎强势, m_User))
+				if (TryBuff(m_Assist, m_User, Skill.强震魔法) ||
+				    TryBuff(m_Assist, m_User, Skill.神圣战甲术) ||
+				    TryBuff(m_Assist, m_User, Skill.幽灵盾) ||
+				    m_User.Class != PlayerClass.Mage && TryBuff(m_Assist, m_User, Skill.猛虎强势))
 					return 1;
-
-				var skill = m_Assist.GetSkill(Skill.施毒术);
-				if (skill.IsValid)
-				{
-					int targetId = m_User.SkillTarget.Value;
-					if (targetId != 0)
-					{
-						var target = m_Assist.GetUnit(targetId);
-						if (target.IsValid && target.Hp > 0)
-						{
-							if (target.Type == UnitType.Monster && target.MaxHp >= 1500)
-							{
-								if (!target.RedPoison && m_Assist.TryCastSkill(Skill.施毒术, target, SkillPoison.Red) ||
-								    !target.GreenPoison && m_Assist.TryCastSkill(Skill.施毒术, target, SkillPoison.Green))
-									return 1;
-							}
-						}
-					}
-				}
 
 				if (TryHeal(m_Assist, m_User, ref m_HealUserTime)) return 1;
 			}
 
-			if (TryCastBuff(m_Assist, Skill.神圣战甲术, m_Assist) ||
-			    TryCastBuff(m_Assist, Skill.幽灵盾, m_Assist)) return 1;
+			if (TryBuff(m_Assist, m_Assist, Skill.神圣战甲术) ||
+			    TryBuff(m_Assist, m_Assist, Skill.幽灵盾)) return 1;
 
+			if (m_HasUser && TryPoison(m_Assist, m_User)) return 1;
 			return 0.5;
 		}
 
-		bool TryHeal(Game self, Game target, ref DateTime healTime)
+		bool TryHeal(Game self, Game target, ref DateTime healTime, int n = 20, int m = 50)
 		{
 			var skill = self.GetSkill(Skill.治愈术);
 			if (skill.IsValid)
@@ -95,12 +77,59 @@
 			return false;
 		}
 
-		bool TryCastBuff(Game self, Skill skill, Game target)
+		bool TryBuff(Game self, Game target, Skill skill)
 		{
-			var s = self.GetSkill(skill);
-			return s.IsValid &&
-			       target.Buff.CanReceive(skill, s.Amulet) &&
-			       self.TryCastSkill(skill, target.Self);
+			var data = self.GetSkill(skill);
+			return data.IsValid && target.Buff.CanReceive(skill, data.Amulet) && self.TryCastSkill(skill, target.Self);
+		}
+
+		int m_PoisonTarget;
+		DateTime m_RedPoisonTime;
+		DateTime m_GreenPoisonTime;
+
+		bool TryPoison(Game self, Game unit)
+		{
+			var skill = self.GetSkill(Skill.施毒术);
+			if (!skill.IsValid) return false;
+			int targetId = 0;
+			if (unit.Casting) targetId = unit.SkillTarget;
+			if (targetId == 0 && unit.Attacking) targetId = unit.AttackTarget;
+			if (targetId == 0) return false;
+			if (m_PoisonTarget != targetId)
+			{
+				m_PoisonTarget = targetId;
+				m_RedPoisonTime = default;
+				m_GreenPoisonTime = default;
+			}
+
+			var target = self.GetUnit(targetId);
+			if (!target.IsValid || target.Hp == 0) return false;
+			var targetType = target.Type;
+			if (targetType == UnitType.Player)
+			{
+				if (unit.AttackMode == AttackMode.Peace) return false;
+				if (self.AttackMode == AttackMode.Peace) self.AttackMode.Set(AttackMode.Group);
+			}
+			else if (targetType == UnitType.Monster)
+			{
+				if (target.MaxHp <= 1200) return false;
+			}
+			else return false;
+
+			// TODO
+			var poison = SkillPoison.None;
+			if (m_Now >= m_RedPoisonTime && !target.RedPoison) poison = SkillPoison.Red;
+			if (poison == SkillPoison.None && m_Now >= m_GreenPoisonTime && !target.GreenPoison)
+				poison = SkillPoison.Green;
+			if (poison == SkillPoison.None) return false;
+			if (self.TryCastSkill(Skill.施毒术, target, poison))
+			{
+				if (poison == SkillPoison.Red) m_RedPoisonTime = m_Now + TimeSpan.FromSeconds(20);
+				else if (poison == SkillPoison.Green) m_GreenPoisonTime = m_Now + TimeSpan.FromSeconds(20);
+				return true;
+			}
+
+			return false;
 		}
 	}
 }
